0993c9c5e24d1bfea551bf2936584c13aedb201d
drkwng <iamdrkwng@gmail.com>
2022-04-07 16:17:21 +0200

    Fix writing results bug

--- main.py
+++ main.py
@@ -10,35 +10,35 @@ from parser_dreame.parser import DreameParser
 
 
 def csv_writer(filename, mode, data):
-    with open(filename, 'w' if mode == 0 else 'a', encoding='utf-8', newline='') as res_file:
+    with open(filename, mode, encoding='utf-8', newline='') as res_file:
         writer = csv.writer(res_file, delimiter=';')
-        if mode == 0:
-            heading = ['AUTHOR_LINK', 'BOOK_SEEN']
-            writer.writerow(heading)
         writer.writerow(data)
 
 
 if __name__ == "__main__":
-    path = os.getcwd()
-    result_path = path + '\\results\\result.csv'
+    # path = os.getcwd()
+    result_path = 'results/result.csv'
 
     urls_file = input('Enter URLs filename: ').strip()
     with open(urls_file, 'r', encoding='utf-8') as f:
         urls = [line.strip() for line in f]
 
+    csv_heading = ['AUTHOR_LINK', 'BOOK_SEEN']
+    csv_writer(result_path, 'w', csv_heading)
+
     parser = DreameParser()
 
     while True:
-        for num, url in enumerate(urls):
+        for url in urls:
             if 'http' in url:
                 try:
                     for work in parser.worker(url):
                         print(f'Found data {work} \n'
                               f'Writing to CSV file')
-                        csv_writer(result_path, num, work)
+                        csv_writer(result_path, 'a', work)
 
                 except Exception as err:
-                    print('Main loop err: ', err, type(err))
+                    print('Main loop exception: ', err, type(err))
 
         break
 
